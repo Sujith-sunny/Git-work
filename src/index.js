0abe9df3d81ab2aa8e30be0f41bb80eb685ee2dd
Sujith Joseph <sunny.perikala@gmail.com>
2023-08-20 16:15:15 +0530

    Version 1 Updated and git ignore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 console.log("This is Index File")
 console.log("version 1")
 
-console.log("This is for version 2")
-
-console.log("This is for version 3")
+console.log("This is restored and changed to v1 updated")
